a4108815cb59f21d3e709a658f69da82f0e859f7
Senavirathna05 <sahansenavirathna05@gmail.com>
2024-11-23 21:32:55 +0530

    1

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './shared/Navbar';
+import './shared/Navbar.css'
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar/>
     </div>
   );
 }
--- portfolio/src/shared/Navbar.js
+++ portfolio/src/shared/Navbar.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import './Navbar.css';
+
 
 
 function Navbar() {
   return (
-    <div>
-      
+    <div className='Header'>
+    <div className='title'>
+        <h1>Dashboard</h1>
     </div>
+    <div className='information'>
+        <div className='pa'>
+            <p>Today's Income: <span className='textboltd'>RS.10,000</span></p>
+        </div>
+        <div className='income'>
+             <i className='material-icons notifiicon'>notifications</i>
+             
+        </div>
+    </div>
+</div>
   )
 }
 
