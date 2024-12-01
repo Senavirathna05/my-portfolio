1f815f1b76a24248d6fa93831c965b9b940ed7ac
Senavirathna05 <sahansenavirathna05@gmail.com>
2024-12-01 22:09:24 +0530

    1

--- portfolio/src/components/Services/Services.js
+++ portfolio/src/components/Services/Services.js
@@ -14,6 +14,22 @@ function Services() {
           <a href='#' className='btn'>Read More</a>
         </div>
       </div>
+
+      <div className='services-box'>
+          <box-icon name='code-alt'></box-icon>
+          <h3>Graphic Design</h3>
+          <p> If the program doesn't work the way it should, the programmer should use
+          testing tools and methods to find and fix the mistakes in the code.</p>
+          <a href='#' className='btn'>Read More</a>
+        </div>
+
+        <div className='services-box'>
+          <box-icon name='code-alt'></box-icon>
+          <h3>UI/UX Design</h3>
+          <p> If the program doesn't work the way it should, the programmer should use
+          testing tools and methods to find and fix the mistakes in the code.</p>
+          <a href='#' className='btn'>Read More</a>
+        </div>
        
     </section>
   )
