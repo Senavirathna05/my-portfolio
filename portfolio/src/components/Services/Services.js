cda93186f8038cc9d92feb4f64d786bc9dcfb89c
Senavirathna05 <sahansenavirathna05@gmail.com>
2024-12-01 21:05:20 +0530

    service form

--- portfolio/src/components/Services/Services.js
+++ portfolio/src/components/Services/Services.js
@@ -3,13 +3,17 @@ import React from 'react'
 function Services() {
   return (
     <section className='services' id='services'>
-        <h2 className='heading'>my <span>Services</span></h2>
+      <h2 className='heading'>my <span>Services</span></h2>
 
-        <div className='services-container'>
-            <div className='services-box'>
-            <box-icon name='code-alt'></box-icon>
-            </div>
+      <div className='services-container'>
+        <div className='services-box'>
+          <box-icon name='code-alt'></box-icon>
+          <h3>Web Development</h3>
+          <p> If the program doesn't work the way it should, the programmer should use
+          testing tools and methods to find and fix the mistakes in the code.</p>
+          <a href='#' className='btn'>Read More</a>
         </div>
+      </div>
        
     </section>
   )
