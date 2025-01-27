8ba6d3ca48fe9d205207fafb62c07e862f97d576
Senavirathna05 <sahansenavirathna05@gmail.com>
2025-01-28 01:43:29 +0530

    1

--- portfolio/src/components/Services/Services.js
+++ portfolio/src/components/Services/Services.js
@@ -4,31 +4,32 @@ import './Services.css';
 function Services() {
   return (
     <section className='services' id='services'>
-      <h2 className='heading'>my <span>Services</span></h2>
+     
+      <h2 className='services-heading'>my <span>Services</span></h2>
 
       <div className='services-container'>
         <div className='services-box'>
-          <box-icon name='code-alt'></box-icon>
+          <i class='bx bx-code-alt'> </i>
           <h3>Web Development</h3>
           <p> If the program doesn't work the way it should, the programmer should use
           testing tools and methods to find and fix the mistakes in the code.</p>
-          <a href='#' className='btn'>Read More</a>
+          <a href='#' className='services-btn'>Read More</a>
         </div>
 
         <div className='services-box'>
-          <box-icon name='palette'></box-icon>
+          <i class='bx bx-palette'></i>
           <h3>Graphic Design</h3>
           <p> If the program doesn't work the way it should, the programmer should use
           testing tools and methods to find and fix the mistakes in the code.</p>
-          <a href='#' className='btn'>Read More</a>
+          <a href='#' className='services-btn'>Read More</a>
         </div>
 
         <div className='services-box'>
-          <box-icon name='paint' type='solid' ></box-icon>
+          <i class='bx bx-paint'></i>
           <h3>UI/UX Design</h3>
           <p> If the program doesn't work the way it should, the programmer should use
           testing tools and methods to find and fix the mistakes in the code.</p>
-          <a href='#' className='btn'>Read More</a>
+          <a href='#' className='services-btn'>Read More</a>
         </div>
       </div>
        
